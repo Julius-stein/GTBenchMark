072080c477cb376df59ae174b50c4219c98fd88b
Julius-stein <381058973@qq.com>
2025-10-20 20:33:04 +0800

    clean re

--- day1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e  # 遇到错误自动停止
-echo "=== $(date): Start job 1 ==="
-python main.py --cfg configs/GPS/zinc-GPS.yaml 
-
-echo "=== $(date): Job 1 finished, start job 2 ==="
-python main.py --cfg configs/Exphormer/zinc-Exphormer.yaml 
